36cc59311aed947821a45ea265ccdf282a268fc0
Shania <shaniaantezana@gmail.com>
2022-12-28 12:04:42 -0400

    created product.model.js

---
+++ models/category.model.js
@@ -0,0 +1,5 @@
+const { Schema, model} = require('mongoose')
+const categorySchema = new Schema({
+    name:{type: String,required: [true,'name is required'],lowercase: true,trim: true,unique: true}
+})
+module.exports = model(categorySchema,'Category')
