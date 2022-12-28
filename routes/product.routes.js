14f46544332999afea4f6426ee12dc7dad6ee875
Shania <shaniaantezana@gmail.com>
2022-12-28 13:03:54 -0400

    created folder routes and file product.routes.js

---
+++ routes/product.routes.js
@@ -0,0 +1,12 @@
+const {Router} = require('express')
+const { Product } = require('../models/product.model')
+const router = Router()
+router.get('/product',async(req,res)=>{
+    try {
+       const products = await Product.find()
+       res.status(200).json(products)
+
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
