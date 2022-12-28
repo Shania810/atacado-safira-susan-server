ee2f96f035c0bb31c541e05af85b853193c088a8
Shania <shaniaantezana@gmail.com>
2022-12-28 11:39:45 -0400

    connection with database

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
+const  connectDB = require('./db/db.config')
+connectDB()
 const app = express()
 app.use(morgan('dev'))
 app.get('/',(req,res)=> res.send('deu certo'))
