9820ae2facd084a4226e5b7ea4e930afed0bbf34
Shania <shaniaantezana@gmail.com>
2022-12-28 18:09:20 -0400

    inserted new property in model order in order.model.js

--- models/order.model.js
+++ models/order.model.js
@@ -2,5 +2,5 @@ const { Schema, model} = require('mongoose')
 const orderSchema = new Schema({
    order_items:[{type: Schema.Types.ObjectId,ref:'Item'}],
    seller:{type: String}
-})
+},{timestamps: true})
 module.exports = model('Order',orderSchema)
