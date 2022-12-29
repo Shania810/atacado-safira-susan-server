4a368be2c8d7882a321b9f5c0ddcf4ea5802b67c
Shania <shaniaantezana@gmail.com>
2022-12-29 18:41:38 -0400

    change route order method get in order.routes.js

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -3,7 +3,13 @@ const Order = require('../models/order.model')
 const router = Router()
 router.get('/order', async (req, res) => {
     try {
-        const orders = await Order.find({}).sort({ createdAt: -1 })
+        const orders = await Order.find({}).sort({ createdAt: -1 }).populate({
+            path: 'order_items',
+            populate: {
+                path: 'product',
+                model: 'Product'
+            }
+        })
         res.status(200).json(orders)
     } catch (error) {
         res.status(500).json({ message: error.message })
