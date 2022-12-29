19ef28499eb90cf033c31871102595c6e95ce652
Shania <shaniaantezana@gmail.com>
2022-12-29 11:52:44 -0400

    created route order method post in order.routes.js

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -9,4 +9,13 @@ router.get('/order',async(req,res)=>{
     res.status(500).json({message: error.message})
    }
 })
+router.post('/order',async(req,res)=>{
+    const order = req.body
+    try {
+      const newOrder = await Order.create(order)
+      res.status(200).json(newOrder)
+    } catch (error) {
+       res.status(500).json({message: error.message})
+    }
+})
 module.exports = router
