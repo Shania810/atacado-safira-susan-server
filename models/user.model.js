a6482c0f4d02c5d398a0f52f7f293ea00e80c533
Shania <shaniaantezana@gmail.com>
2023-01-06 21:28:18 -0400

    create file user.model.js

---
+++ models/user.model.js
@@ -0,0 +1,9 @@
+const { Schema, model } = require('mongoose')
+
+const userSchema = new Schema({
+    name: { type: String, required: [true, 'name is required'], trim: true, lowercase: true, unique: true },
+    passwordHash: { type: String, required: [true, 'passwordHash is required'] },
+    role: { type: String, required: [true, 'role is required'], trim: true }
+})
+
+module.exports = model(userSchema)
