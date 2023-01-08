b2ddb59f5a4f17abac93b40d7a9ff0c8294b4e20
Shania <shaniaantezana@gmail.com>
2023-01-08 13:36:04 -0400

    change order.model.js

--- models/order.model.js
+++ models/order.model.js
@@ -3,7 +3,6 @@ const orderSchema = new Schema({
    order_items: [{
       product: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
       quantity: { type: Number, required: true },
-      total: {type: Number,required:true}
    }],
    seller: {type: Schema.Types.ObjectId, ref: 'User',required: true}
 }, { timestamps: true })
