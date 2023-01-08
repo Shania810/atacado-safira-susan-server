77c7d127f83df956db1a88629787d8bbbb79d034
Shania <shaniaantezana@gmail.com>
2023-01-08 14:00:01 -0400

    create file client.model.js

---
+++ models/client.model.js
@@ -0,0 +1,11 @@
+const { Schema, model } = require('mongoose')
+
+const clientSchema = new Schema({
+    name: { type: String, required: [true, 'name is required'], lowercase: true, trim: true, unique: true },
+    address: { type: String, required: [true, 'address is required'], lowercase: true, trim: true },
+    phone: { type: Number, required: [true, 'phone is required'], min: 8, max: 11 },
+    cpf: { type: Number, max: 11 },
+    cnpj: { type: Number, max: 14 }
+})
+
+module.exports = model('Client', clientSchema)
