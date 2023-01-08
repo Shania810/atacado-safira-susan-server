e5682399f8205fea728b3bdfba67760e2aa96492
Shania <shaniaantezana@gmail.com>
2023-01-08 19:00:39 -0400

    change product.model.js

--- models/product.model.js
+++ models/product.model.js
@@ -4,6 +4,7 @@ const productSchema = new Schema({
     category: { type: Schema.Types.ObjectId, ref: 'Category' },
     stock: { type: Number, default: 0 },
     commission_amount:{type:Number,required:[true,'commission amount is required']},
+    price:{type:Number,default: 0,required:[true,'price is required']},
     wholesale_price: { type: Number, default: 0,required:[true,'wholesale price is required'] },
     retail_price: { type: Number, default: 0 ,required:[true,'retail price is required']},
     description: { type: String, required: [true, 'description is required'], lowercase: true, trim: true }
