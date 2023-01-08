77d8860380d40abed5b62f2ea8f89d9b568f76ab
Shania <shaniaantezana@gmail.com>
2023-01-08 13:28:22 -0400

    change product.model.js

--- models/product.model.js
+++ models/product.model.js
@@ -3,8 +3,9 @@ const productSchema = new Schema({
     name: { type: String, required: [true, 'name of product is required'], lowercase: true, trim: true, unique: true },
     category: { type: Schema.Types.ObjectId, ref: 'Category' },
     stock: { type: Number, default: 0 },
-    wholesale_price: { type: Number, default: 0 },
-    retail_price: { type: Number, default: 0 },
+    commission_amount:{type:Number,required:[true,'commission amount is required']},
+    wholesale_price: { type: Number, default: 0,required:[true,'wholesale price is required'] },
+    retail_price: { type: Number, default: 0 ,required:[true,'retail price is required']},
     description: { type: String, required: [true, 'description is required'], lowercase: true, trim: true }
 })
 module.exports = model('Product', productSchema)
