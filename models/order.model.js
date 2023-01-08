7e759f305844c21d22bd042ccc1ce70b44da0689
Shania <shaniaantezana@gmail.com>
2023-01-08 16:45:25 -0400

    change order.model.js

--- models/order.model.js
+++ models/order.model.js
@@ -1,9 +1,10 @@
 const { Schema, model } = require('mongoose')
 const orderSchema = new Schema({
+   client: { type: Schema.Types.ObjectId, ref: 'Client', required: true },
    order_items: [{
       product: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
       quantity: { type: Number, required: true },
    }],
-   seller: {type: Schema.Types.ObjectId, ref: 'User',required: true}
+   seller: { type: Schema.Types.ObjectId, ref: 'User', required: true }
 }, { timestamps: true })
 module.exports = model('Order', orderSchema)
